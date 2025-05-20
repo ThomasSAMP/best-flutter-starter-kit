67e238ec9dfb04a4afc54003a0f8d6bb3fc34f58
ThomasSAMP <lol.loli@outlook.fr>
2025-05-20 13:30:42 +0200

    update theme provider for conflicts

--- lib/shared/providers/theme_provider.dart
+++ lib/shared/providers/theme_provider.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-enum ThemeMode { light, dark, system }
+enum AppThemeMode { light, dark, system }
 
-class ThemeNotifier extends StateNotifier<ThemeMode> {
-  ThemeNotifier() : super(ThemeMode.system) {
+class ThemeNotifier extends StateNotifier<AppThemeMode> {
+  ThemeNotifier() : super(AppThemeMode.system) {
     _loadTheme();
   }
 
@@ -15,41 +15,53 @@ class ThemeNotifier extends StateNotifier<ThemeMode> {
     final prefs = await SharedPreferences.getInstance();
     final themeIndex = prefs.getInt(_themeKey);
     if (themeIndex != null) {
-      state = ThemeMode.values[themeIndex];
+      state = AppThemeMode.values[themeIndex];
     }
   }
 
-  Future<void> setThemeMode(ThemeMode mode) async {
+  Future<void> setThemeMode(AppThemeMode mode) async {
     state = mode;
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt(_themeKey, mode.index);
   }
 }
 
-final themeProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>((ref) {
+final themeProvider = StateNotifierProvider<ThemeNotifier, AppThemeMode>((ref) {
   return ThemeNotifier();
 });
 
-extension ThemeModeExtension on ThemeMode {
+// Mettez à jour l'extension également
+extension AppThemeModeExtension on AppThemeMode {
   String get name {
     switch (this) {
-      case ThemeMode.light:
+      case AppThemeMode.light:
         return 'Light';
-      case ThemeMode.dark:
+      case AppThemeMode.dark:
         return 'Dark';
-      case ThemeMode.system:
+      case AppThemeMode.system:
         return 'System';
     }
   }
 
   IconData get icon {
     switch (this) {
-      case ThemeMode.light:
+      case AppThemeMode.light:
         return Icons.wb_sunny_outlined;
-      case ThemeMode.dark:
+      case AppThemeMode.dark:
         return Icons.nights_stay_outlined;
-      case ThemeMode.system:
+      case AppThemeMode.system:
         return Icons.settings_outlined;
+    }
+  }
+
+  ThemeMode toThemeMode() {
+    switch (this) {
+      case AppThemeMode.light:
+        return ThemeMode.light;
+      case AppThemeMode.dark:
+        return ThemeMode.dark;
+      case AppThemeMode.system:
+        return ThemeMode.system;
     }
   }
 }
