52eaa968bf77ffe40e632ca539e924d785a6f6f9
ThomasSAMP <lol.loli@outlook.fr>
2025-05-20 13:14:40 +0200

    init dependencies injection

---
+++ lib/core/di/register_module.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../config/env_config.dart';
+import '../network/dio_client.dart';
+
+@module
+abstract class RegisterModule {
+  @preResolve
+  Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
+
+  @lazySingleton
+  FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
+
+  @lazySingleton
+  FirebaseFirestore get firestore => FirebaseFirestore.instance;
+
+  @lazySingleton
+  FirebaseStorage get storage => FirebaseStorage.instance;
+
+  @lazySingleton
+  Dio get dio => DioClient.createDio(
+    baseUrl: EnvConfig.instance.apiUrl,
+    enableLogging: EnvConfig.instance.enableLogging,
+  );
+}
